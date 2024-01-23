2d13bcdd3c258f392809d53c8936813bdf8e8827
PhusseinP <150959811+PhusseinP@users.noreply.github.com>
2024-01-23 14:01:40 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/*hussein  1/12 */
 public class Main {
     public static void main(String[] args) {
 
-        /*System.out.println("Hello world!");
-        int age = 18;
-        double gpa = 3.8;
-        boolean isRaining = false;
-        System.out.println(age + "|" + gpa + "|" + isRaining);*/
-        String city = "Columbus";
-        int zipcode = 43215;
-        double weather[] = {32, 25, 27,40,45};
-        System.out.println("city" + city);
-        System.out.println();
 
+        String city = "Columbus";
+        int zipCode = 43215;
+        double weather[] = {32, 25, 27, 40, 45};
+        System.out.println("city " + city);
+        System.out.println(zipCode);
+
+        double sum = 0;
+
+
+        for (int i = 0; i < weather.length; i++) {
+            sum += weather[i]/weather.length;
+        }
+        System.out.println(sum);
     }
 }
